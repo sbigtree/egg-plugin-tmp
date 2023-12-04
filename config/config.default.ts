f6248a5f729a2286acb96853a3739d3995da0c68
qzuser <83346348@qq.com>
2023-12-04 12:03:31 +0800

    a

--- config/config.default.ts
+++ config/config.default.ts
@@ -14,6 +14,8 @@ export default (appInfo: EggAppInfo) => {
   // add your egg config in here
   // config.middleware = [];
 
+  // @ts-ignore
+  config.security = {csrf: {enable: false}}
   // add your special config in here
   const bizConfig = {
     sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
